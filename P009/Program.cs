aee8276d043270446bd26954b0d62039c0c498a7
SpencasaurusRex <SpencasaurusRex@gmail.com>
2021-06-28 16:17:05 -0400

    Problem 9

---
+++ P009/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Generate all possible values of a,b,c then check for validity
+        int number = 0;
+        for (int c = 335; c <= 997; c++) {
+            int r = 1000 - c;
+            for (int b = c - 1; b > r / 2; b--) {
+                int a = r - b;
+                if (c * c - b * b - a * a == 0) {
+                    Console.WriteLine($"{a}*{b}*{c} = {a * b * c}");
+                    return;
+                }
+            }
+        }
+    }
+}
