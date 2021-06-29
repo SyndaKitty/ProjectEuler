90096bfa78865273a36a97720d2415b15d3fe0dc
SpencasaurusRex <SpencasaurusRex@gmail.com>
2021-06-29 12:20:25 -0400

    Problem 20

---
+++ P020-Factorial_digit_sum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+class Program {
+    static void Main(string[] args) {
+        BigInteger product = new BigInteger(1);
+        for (int i = 2; i <= 99; i++) {
+            product *= i;
+        }
+
+        Console.WriteLine(product.ToString().ToCharArray().Sum(w => (w - '0')));
+    }
+}
