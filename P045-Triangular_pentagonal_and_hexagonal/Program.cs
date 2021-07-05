53a6bb8f9c4e54e2f16d1e5bb33ffc45e86c0f3b
SpencasaurusRex <SpencasaurusRex@gmail.com>
2021-07-05 10:47:38 -0400

    Improved Problem 45

--- P045-Triangular_pentagonal_and_hexagonal/Program.cs
+++ P045-Triangular_pentagonal_and_hexagonal/Program.cs
@@ -2,19 +2,14 @@ using System;
 
 class Program {
     static void Main(string[] args) {
-        decimal tn = 2; decimal t = 3;
         decimal pn = 2; decimal p = 5;
         for (int n = 144;; n++) {
             decimal h = 2 * n * n - n;
-            while (t < h) {
-                tn++;
-                t = (tn * tn + tn) / 2;
-            }
             while (p < h) {
                 pn++;
                 p = (3 * pn * pn - pn) / 2;
             }
-            if (h == t && h == p) {
+            if (h == p) {
                 Console.WriteLine(h);
                 return;
             }
